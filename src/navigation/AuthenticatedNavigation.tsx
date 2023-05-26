c9c42026b15913aba572d2f04f156d84c03d175a
Cosmin-Madalin Stan <cosmin@hedia.co>
2023-05-26 10:58:31 +0200

    rename

--- src/navigation/AuthenticatedNavigation.tsx
+++ src/navigation/AuthenticatedNavigation.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import BottomTabNavigator from './BottomTabNavigation';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import BottomTabNavigator from "./BottomTabNavigator";
 
-const {Navigator, Screen, Group} = createNativeStackNavigator();
+const { Navigator, Screen, Group } = createNativeStackNavigator();
 
 const AuthenticatedNavigation = () => {
   return (
-    <Navigator screenOptions={{headerShown: false}}>
-      <Group screenOptions={{presentation: 'card'}}>
-        <Screen name={'BottomTab'} component={BottomTabNavigator} />
+    <Navigator screenOptions={{ headerShown: false }}>
+      <Group screenOptions={{ presentation: "card" }}>
+        <Screen name={"BottomTab"} component={BottomTabNavigator} />
       </Group>
     </Navigator>
   );
--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -6,11 +6,11 @@ import BottomTab from "./BottomTab";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 const BottomTabNavigator = () => {
-	return (
-		<Navigator screenOptions={{ headerShown: false }} tabBar={BottomTab}>
-			<Screen name="Home" component={HomeScreen} />
-			<Screen name="Scoreboard" component={ScoreboardScreen} />
-		</Navigator>
-	);
+  return (
+    <Navigator screenOptions={{ headerShown: false }} tabBar={BottomTab}>
+      <Screen name="Home" component={HomeScreen} />
+      <Screen name="Scoreboard" component={ScoreboardScreen} />
+    </Navigator>
+  );
 };
 export default BottomTabNavigator;
